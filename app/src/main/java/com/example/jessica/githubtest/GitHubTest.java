215e47669e9dc1ef2ff705ad6fc18a88b411d08e
jess <J>
2016-12-06 20:17:43 +0000

    Initial commit

---
+++ app/src/main/java/com/example/jessica/githubtest/GitHubTest.java
@@ -0,0 +1,13 @@
+package com.example.jessica.githubtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GitHubTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git_hub_test);
+    }
+}
